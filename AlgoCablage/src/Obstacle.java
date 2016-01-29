e63ca0683811441ba34630aa898e418cd0c5b7cf
heraknos <heraknoshsdj@hotmail.fr>
2016-01-29 15:22:35 +0100

    Base de travail sprint 1
    Une map contenant 4 objets et des bords

--- AlgoCablage/src/Obstacle.java
+++ AlgoCablage/src/Obstacle.java
@@ -3,6 +3,9 @@ import java.util.List;
 public class Obstacle {
 	private List<Coord> sommets;
 
+	public Obstacle(List<Coord> sommets) {
+		this.sommets = sommets;
+	}
 	public List<Coord> getSommets() {
 		return sommets;
 	}
